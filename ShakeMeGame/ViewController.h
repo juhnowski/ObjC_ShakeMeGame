c0cce610fde60552304a47bc2357b59c56b3addc
Juhnowski <juhnowski@gmail.com>
2018-05-02 03:20:35 +0300

    initial

--- ShakeMeGame/ViewController.h
+++ ShakeMeGame/ViewController.h
@@ -8,8 +8,20 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController {
+    
+    NSTimer *timer;
+    int countInt;
+    
+    int scoreInt;
+    
+    int modeInt;
+}
+@property (weak, nonatomic) IBOutlet UILabel *timerLabel;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UIButton *startButton;
 
+- (IBAction)StartGame:(id)sender;
 
 @end
 
--- ShakeMeGame/ViewController.m
+++ ShakeMeGame/ViewController.m
@@ -17,12 +17,62 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    countInt = 10;
+    scoreInt = 0;
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)StartGame:(id)sender {
+    if (scoreInt == 0) {
+        countInt = 10;
+        
+        modeInt = 1;
+        
+        self.timerLabel.text = [NSString stringWithFormat:@"%i", countInt];
+        
+        timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(startCounter) userInfo:nil repeats:YES];
+        
+        self.startButton.enabled = NO;
+    }
+    
+    if (countInt == 0) {
+        scoreInt = 0;
+        countInt = 10;
+        self.scoreLabel.text = [NSString stringWithFormat:@"%i", scoreInt];
+        self.timerLabel.text = [NSString stringWithFormat:@"%i", countInt];
+        
+        [self.startButton setTitle:@"Start" forState:UIControlStateNormal];
+    }
+}
+
+-(void) startCounter{
+    countInt -= 1;
+    self.timerLabel.text = [NSString stringWithFormat:@"%i", countInt];
+    
+    if (countInt == 0) {
+        [timer invalidate];
+        
+        modeInt = 2;
+        
+        [self.startButton setTitle:@"Restart" forState:UIControlStateNormal];
+        self.startButton.enabled = YES;
+    }
+}
+
+-(void)motionBegan:(UIEventSubtype)motion withEvent:(UIEvent *)event{
+    if (event.subtype == UIEventSubtypeMotionShake) {
+        if (modeInt == 1) {
+            scoreInt += 1;
+            
+            self.scoreLabel.text = [NSString stringWithFormat:@"%i", scoreInt];
+        }
+    }
 }
 
 
